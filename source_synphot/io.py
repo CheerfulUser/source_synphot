13f139bf53fa4fec1005848229214a79efa8f6ea
Gautham Narayan <gsnarayan@gmail.com>
2017-07-25 16:36:59 -0400

    Expanded passband and spectrum loading routines

--- source_synphot/io.py
+++ source_synphot/io.py
@@ -10,13 +10,15 @@ from __future__ import unicode_literals
 import sys
 import os
 import argparse
+import pkg_resources
 import astropy.table as at
 import pysynphot as S
+
 
 def get_options(args=None):
     """
     Get command line options
-    
+
     Parameters
     ----------
     args : None or array-like
@@ -73,12 +75,90 @@ def get_options(args=None):
         raise ValueError(message)
 
     return args
-    
+
+
+def get_pkgfile(infile):
+    """
+    Returns the full path to a file inside the :py:mod:`source_synphot` package
+
+    Parameters
+    ----------
+    infile : str
+        The name of the file to set the full package filename for
+
+    Returns
+    -------
+    pkgfile : str
+        The path to the file within the package.
+
+    Raises
+    ------
+    IOError
+        If the ``pkgfile`` could not be found inside the :py:mod:`source_synphot` package.
+
+    Notes
+    -----
+        This allows the package to be installed anywhere, and the code to still
+        determine the location to a file included with the package, such as the
+        passband definition file
+    """
+
+    pkgfile = pkg_resources.resource_filename('source_synphot',infile)
+
+    if not os.path.exists(pkgfile):
+        message = 'Could not find package file {}'.format(pkgfile)
+        raise IOError(message)
+    return pkgfile
+
+
+def get_passband(pb):
+    """
+    Read a passband
+
+    Parameters
+    ----------
+    sourcepb : str
+        pysynphot obsmode or obsmode listed in `pbzptmag.txt`
+
+    Returns
+    -------
+    pb : :py:class:`numpy.recarray`
+        Record array with the passband data.
+        Has ``dtype=[('wave', '<f8'), ('transmission', '<f8')]``
+
+    See Also
+    --------
+    :py:func:`astropy.table.Table.read`
+    """
+    zptmag = np.nan
+    try:
+        out = S.ObsBandpass(pb)
+    except ValueError as e:
+        infile = os.path.join('passbands','pbzptmag.txt')
+        pbzptfile = get_pkgfile(infile)
+        pbzpt = at.Table.read(pbzptfile, format='ascii')
+        ind = (pbzpt.obsmode == pb)
+        if len(pbzpt.passband[ind]) == 1:
+            if not np.isnan(pbzpt.zpt[ind][0]):
+                pb = pbzpt.passband[ind][0]
+                zptmag = pbzpt.zpt[ind][0]
+            else:
+                # we got a passband, but not a zeropoint, which is pretty useless
+                message = 'Passband {} is listed in pbzptmag file, but zeropoint is NaN. Cannot use.'.format(pb)
+                raise RuntimeError(message)
+        else:
+            # pb is not unique
+            message = 'Passband {} is not uniquely listed in pbzptmag file.'.format(pb)
+            raise RuntimeError(message)
+
+        if os.path.exists(pb):
+            pbdata = at.Table.read(pb, names=('wave','transmission'))
+
 
 def get_source(sourcespec):
     """
-    Read a spectrum 
-    
+    Read a spectrum
+
     Parameters
     ----------
     sourcespec : str
@@ -88,23 +168,33 @@ def get_source(sourcespec):
          * BB<Temperature>
          * PL<Reference Wave Angstrom>_<PL Index>
          * Flat<AB magnitude>
+         * ckmod<Temperature>_<logZ>_<logg>
+
     Returns
     -------
     source : :py:class:`numpy.recarray`
-        Record array with the spectrum data. 
+        Record array with the spectrum data.
         Has ``dtype=[('wave', '<f8'), ('flux', '<f8'), ('flux_err', '<f8')]``
         `flux_err` is set to 0. for pre-defined sources.
+
+    Notes
+    -----
+       The source spectrum specification string parsing is basic. Strings are
+       checked to see if they start with an acceptable prefix, and then split.
+       The individual constituents are converted to float, and passed to
+       pysynphot. There is no checking of the values, only that they can be
+       properly typecast into float. Making sure the value is accepted is left
+       to the user.
 
     See Also
     --------
     :py:func:`astropy.table.Table.read`
-    :py:func:`numpy.genfromtxt`
-    :py:func:`_read_ascii`
     """
     if os.path.exists(sourcespec):
         spec = at.Table.read(sourcespec, names=('wave','flux','dflux'))
     else:
-        # assume sourcespec is a string and parse it
+        # assume sourcespec is a string and parse it, trying to interpret as:
+        # simple blackbody spectrum
         if sourcespec.startswith('BB'):
             temp = sourcespec.lstrip('BB')
             try:
@@ -114,6 +204,8 @@ def get_source(sourcespec):
                 raise ValueError(message)
             bb = S.Blackbody(temp)
             spec = {'wave':bb.wave, 'flux':bb.flux, 'dflux':np.repeat(0., len(bb.wave))}
+
+        # power-law spectrum
         elif sourcespec.startswith('PL'):
             refwave, plindex = sourcespec.lstrip('PL').split('_')
             try:
@@ -128,6 +220,8 @@ def get_source(sourcespec):
                 raise ValueError(message)
             pl = S.PowerLaw(refwave, plindex)
             spec = {'wave':pl.wave, 'flux':pl.flux, 'dflux':np.repeat(0., len(pl.wave))}
+
+        # flat spectrum (in f_lam, not f_nu) normalized to some ABmag
         elif sourcespec.startswith('Flat'):
             abmag = sourcespec.replace('Flat','')
             try:
@@ -137,9 +231,32 @@ def get_source(sourcespec):
                 raise ValueError(message)
             flat = S.FlatSpectrum(abmag, fluxunits='abmag')
             spec = {'wave':flat.wave, 'flux':flat.flux, 'dflux':np.repeat(0., len(flat.wave))}
+
+        # a Castelli-Kurucz model
+        elif sourcespec.startswith('ckmod'):
+            teff, logZ, logg = sourcespec.replace('ckmod','').split('_')
+            try:
+                teff = float(teff)
+            except (TypeError, ValueError) as e:
+                message = 'Source temperature {} cannot be interpreted as a float'.format(sourcespec)
+                raise ValueError(message)
+            try:
+                logZ = float(logZ)
+            except (TypeError, ValueError) as e:
+                message = 'Abundance {} cannot be interpreted as a float'.format(sourcespec)
+                raise ValueError(message)
+            try:
+                logg = float(logg)
+            except (TypeError, ValueError) as e:
+                message = 'Surface gravity {} cannot be interpreted as a float'.format(sourcespec)
+                raise ValueError(message)
+            ckmod = S.Icat('ck04models',teff, logZ,logg)
+            sec = {'wave':ckmod.wave, 'flux':ckmod.flux, 'dflux':np.repeat(0., len(ckmod.wave))}
+
+        # else give up
         else:
             message = 'Source spectrum {} cannot be parsed as input file or pre-defined type (BB, PL, Flat)'.format(sourcespec)
             raise ValueError(message)
+
     spec = np.rec.fromarrays((spec['wave'], spec['flux'], spec['dflux']), names='wave,flux,dflux')
     return spec
-
