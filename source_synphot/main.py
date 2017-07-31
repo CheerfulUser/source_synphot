e79949dbb639a4818fc7b509b03bc6f94db458e4
Gautham Narayan <gsnarayan@gmail.com>
2017-07-31 18:05:12 -0400

    create test to load all passbands listed in pbzptmag file

--- source_synphot/main.py
+++ source_synphot/main.py
@@ -2,29 +2,17 @@
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
+import os
 import numpy as np
 from . import io
 from . import passband
-import pysynphot as S
 
 def main(inargs=None):
-    args = io.get_options(args=inargs)
+    pbzptfile = os.path.join('passbands','pbzptmag.txt')
+    pbzptfile = io.get_pkgfile(pbzptfile)
+    pbzpt = np.recfromtxt(pbzptfile, names=True)
+    pbnames  = pbzpt.obsmode
+    pbnames = [x.decode('latin-1') for x in pbnames]
 
-    sourcepb, sourcepbzp = io.get_passband(args.sourcepb)
-    if sourcepbzp is None:
-        sourcepbzp = np.nan
-
-    new_sourcepbzp = passband.get_pb_zpt(sourcepb, model_mag=0.)
-    if sourcepbzp != new_sourcepbzp:
-        delta_zp = sourcepbzp - new_sourcepbzp
-        print(delta_zp, sourcepbzp, new_sourcepbzp)
-        sourcepbzp = new_sourcepbzp
-
-    source_spec = S.FlatSpectrum(3631, fluxunits='jy')
-    source_spec.convert('flam')
-
-    ob = S.Observation(source_spec, sourcepb)
-    print(ob.effstim('abmag'))
-
-    out = passband.synphot(source_spec, sourcepb, sourcepbzp)
-    print(out)
+    pbs = passband.get_pbs(pbnames, 0.)
+    print(pbs)
